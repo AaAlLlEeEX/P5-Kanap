46f5912a75eebc1503fa838054c73eb9f12bf9da
AaAllEeEX <delaissealexis@gmail.com>
2023-01-25 11:10:56 +0100

    ajout fichier

--- front/js/script.js
+++ front/js/script.js
@@ -1,32 +1,31 @@
-// Sélection de l'emplacement dans lequel on va afficher nos produits, sur la page d'accueil. Ici dans la section avec l'id "items".
 const sectionItems = document.querySelector('#items');
-// On récupère toutes les données de l'api que l'on met dans un constante listProducts
+
 fetch("http://localhost:3000/api/products")
   .then(response => response.json())
   .then(data => {
     for (const listProducts of data){
       console.log(listProducts);
-      // on cré les éléments html manquants de la page index.html et on y insère les données de l'api
-      let newA = document.createElement('a');
-      newA.setAttribute("href", `./product.html?id=${listProducts._id}`);
-      sectionItems.appendChild(newA);
+      
+      const article = document.createElement('a');
+      article.setAttribute("href", `./product.html?id=${listProducts._id}`);
+      sectionItems.appendChild(article);
 
-      let newArticle = document.createElement('article');
-      newA.appendChild(newArticle);
+      const newArticle = document.createElement('article');
+      article.appendChild(newArticle);
 
-      let newImg = document.createElement('img');
+      const newImg = document.createElement('img');
       newImg.setAttribute("src", listProducts.imageUrl);
       newImg.setAttribute("alt", listProducts.altTxt);
       newArticle.appendChild(newImg);
 
-      let newH3 = document.createElement('h3');
-      newH3.setAttribute("class","productName");
-      newH3.innerText = listProducts.name;
-      newArticle.appendChild(newH3);
+      const titre = document.createElement('h3');
+      titre.setAttribute("class","productName");
+      titre.innerText = listProducts.name;
+      newArticle.appendChild(titre);
       
-      let newP = document.createElement('p');
-      newP.setAttribute("class","productDescription");
-      newP.innerText = listProducts.description;
-      newArticle.appendChild(newP);
+      const text = document.createElement('p');
+      text.setAttribute("class","productDescription");
+      text.innerText = listProducts.description;
+      newArticle.appendChild(text);
     }
    })
